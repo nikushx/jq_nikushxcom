94cc504ee6e2c5c205c2fe9dc83719f3c0fec369
Nikush Dalia <nikushdalia@gmail.com>
2015-12-24 13:24:15 -0500

    Finished first initial site, not fully-responsive

--- js/app.js
+++ js/app.js
@@ -1,12 +1,10 @@
 var main = function () {
     
-    $('.initialBox').animate({
-            
-            left: '50%'
-            
-        }, 200);
+    $('.returnArrow').hide();
+    $('.initialBox').hide();
+    $('.initialBox').fadeIn(1000);
     
-    $('.enter').click( function () {
+    $('.enter').click(function () {
                 
         $('.initialBox').animate({
             
@@ -22,7 +20,13 @@ var main = function () {
         
     });
     
-    $('#1').click( function () {
+    var currentOption = 0;
+    
+    $('#1').click(function () {
+        
+        currentOption = 1;
+        
+        $('.returnArrow').fadeIn(1000);
         
         $('.welcomeBox').animate({
             
@@ -38,7 +42,11 @@ var main = function () {
         
     });
     
-    $('#2').click( function () {
+    $('#2').click(function () {
+        
+        currentOption = 2;
+        
+        $('.returnArrow').fadeIn(1000);
         
         $('.welcomeBox').animate({
             
@@ -54,7 +62,11 @@ var main = function () {
         
     });
     
-    $('#3').click( function () {
+    $('#3').click(function () {
+        
+        currentOption = 3;
+        
+        $('.returnArrow').fadeIn(1000);
         
         $('.welcomeBox').animate({
             
@@ -67,6 +79,57 @@ var main = function () {
             left: '50%'
             
         }, 200);
+        
+    });
+    
+    // Return Arrow Control
+    $('.returnArrow').click(function () {
+        
+        $('.returnArrow').fadeOut(600);
+        
+        if (currentOption === 1) {
+           
+            $('.welcomeBox').animate({
+            
+                left: '50%'
+            
+            }, 200);
+        
+            $('.aboutBox').animate({
+            
+                left: '-125%'
+            
+            }, 200);
+           
+        } else if (currentOption === 2) {
+           
+            $('.welcomeBox').animate({
+            
+                left: '50%'
+            
+            }, 200);
+        
+            $('.expBox').animate({
+            
+                left: '-125%'
+            
+            }, 200);
+           
+        } else if (currentOption === 3) {
+           
+            $('.welcomeBox').animate({
+            
+                left: '50%'
+            
+            }, 200);
+        
+            $('.contactBox').animate({
+            
+                left: '-125%'
+            
+            }, 200);
+           
+        }
         
     });
     
